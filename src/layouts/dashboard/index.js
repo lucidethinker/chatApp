5e626a93388f23e6823d180b40ba6246a34ecdc5
Jasim Ihsan <jasimihsan2@gmail.com>
2023-03-16 11:07:41 +0530

    Bug FIxes ..🪲

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -6,51 +6,79 @@ import Logo from '../../assets/Images/logo.ico';
 import Stack from '@mui/material/Stack';
 import { Nav_Buttons } from "../../data";
 import { Gear } from "phosphor-react";
-
-
+import { faker } from '@faker-js/faker';
+import { Avatar } from '@mui/material';
 const DashboardLayout = () => {
   
   const theme = useTheme();
- 
+  const [selected, setSelected] = React.useState(0);
   return (
-   
     <>
       <Box 
-      p={3}
-      sx={{
-      backgroundColor : theme.palette.background.paper ,
-      boxShadow: '0px 0px 2px rgba(0, 0, 0, 0.25)',
-      height: '100vh',
-      width:100 ,
-    }}>
-    <Stack direction="column" alignItems={"center"} sx={{width:"100%"}} spacing={3}>
-    <Box sx={{
-      backgroundColor : theme.palette.primary.main,
-      height: 64,
-      width: 64,
-      borderRadius:1.5,
-    }}>
-    <img src={Logo} alt="logo"
-     style={{height: 64, width: 64}}/>
-    </Box>
-    <Stack  
-    sc={{width:'max-content'}} 
-    direction="column" 
-    spacing={3} >
-    {Nav_Buttons.map((item) => <IconButton 
-      key={item.id}>
-      {item.icon}
-    </IconButton>)}
-   
-    <Divider />
-      <IconButton>
-      <Gear  />
-      </IconButton>
-     
-
-    </Stack>
-    </Stack>    
-    </Box>
+        p={3}
+        sx={{
+          backgroundColor: theme.palette.background.paper,
+          boxShadow: '0px 0px 2px rgba(0, 0, 0, 0.25)',
+          height: '100vh',
+          width: 100,
+        }}
+      >
+        <Stack 
+          direction="column" 
+          alignItems="center" 
+          sx={{width:"100%"}} 
+          spacing={3}
+        >
+          <Box 
+            sx={{
+              height: 64,
+              width: 64,
+              borderRadius: 1.5,
+            }}
+          >
+            <img 
+              src={Logo} 
+              alt="logo"
+              style={{height: 64, width: 64}}
+            />
+          </Box>
+          <Stack  
+            sc={{width:'max-content'}} 
+            direction="column" 
+            spacing={3} 
+          >
+            {Nav_Buttons.map((item) =>( 
+              item.index === selected ?
+                <Box 
+                  key={item.index}
+                  sx={{
+                    backgroundColor: theme.palette.info.light, 
+                    borderRadius: 1.5,
+                  }}
+                >
+                  <IconButton 
+                    sx={{width:'max-content', color:'#000'}}
+                    onClick={() => setSelected(item.index)}
+                  >  
+                    {item.icon}
+                  </IconButton>
+                </Box>
+              :
+                <IconButton 
+                  key={item.index}
+                  onClick={() => setSelected(item.index)}
+                >
+                  {item.icon}
+                </IconButton>
+            ))}
+            <Divider sx={{width:'48px'}}/>
+            <IconButton>
+              <Gear />
+            </IconButton>
+          </Stack>
+          <Avatar src={faker.image.avatar()} />
+        </Stack>    
+      </Box>
       <Outlet />
     </>
   );
--- src/pages/dashboard/GeneralApp.js
+++ src/pages/dashboard/GeneralApp.js
@@ -1,16 +1,13 @@
 import React , {Suspense,lazy}from "react";
 
-const Cat = lazy(() => import('../../components/Cat'))
+
 
 
 const GeneralApp = () => {
 
   return (
     <>
-    <Suspense fallback={<div>Loading...</div>}>
-      <h1>General App</h1>
-      <Cat />
-      </Suspense>
+    
     </>
   );
 };
