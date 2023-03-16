82be979f668cc6e96df4bad44f9610ba6f35a57b
Jasim Ihsan <jasimihsan2@gmail.com>
2023-03-16 12:32:18 +0530

    Night mode initial commit .. 🌙

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -1,16 +1,69 @@
 import React from "react";
-import {Box, Divider, IconButton} from "@mui/material";
+import {Box, Divider, IconButton,Switch,Avatar} from "@mui/material";
+import { styled } from '@mui/material/styles';
 import { Outlet } from "react-router-dom";
+
 import { useTheme } from "@mui/material/styles";
 import Logo from '../../assets/Images/logo.ico';
 import Stack from '@mui/material/Stack';
 import { Nav_Buttons } from "../../data";
 import { Gear } from "phosphor-react";
 import { faker } from '@faker-js/faker';
-import { Avatar } from '@mui/material';
+
+
+import useSettings from "../../hooks/useSettings"
+
+const AntSwitch = styled(Switch)(({ theme }) => ({
+  width: 40,
+  height: 20,
+  padding: 0,
+  display: 'flex',
+  '&:active': {
+    '& .MuiSwitch-thumb': {
+      width: 15,
+    },
+    '& .MuiSwitch-switchBase.Mui-checked': {
+      transform: 'translateX(9px)',
+    },
+  },
+  '& .MuiSwitch-switchBase': {
+    padding: 2,
+    '&.Mui-checked': {
+      transform: 'translateX(20px)',
+      color: '#fff',
+      '& + .MuiSwitch-track': {
+        opacity: 1,
+        backgroundColor: theme.palette.mode === 'dark' ? '#177ddc' : '#1890ff',
+      },
+    },
+  },
+  '& .MuiSwitch-thumb': {
+    boxShadow: '0 2px 4px 0 rgb(0 35 11 / 20%)',
+    width: 16,
+    height: 16,
+    borderRadius: 8,
+    transition: theme.transitions.create(['width'], {
+      duration: 200,
+    }),
+  },
+  '& .MuiSwitch-track': {
+    borderRadius: 20 / 2,
+    opacity: 1,
+    backgroundColor:
+      theme.palette.mode === 'dark' ? 'rgba(255,255,255,.35)' : 'rgba(0,0,0,.25)',
+    boxSizing: 'border-box',
+  },
+}));
+
+
+
+
+
 const DashboardLayout = () => {
   
   const theme = useTheme();
+  const{onToggleMode}=useSettings();
+
   const [selected, setSelected] = React.useState(0);
   return (
     <>
@@ -80,7 +133,12 @@ const DashboardLayout = () => {
           </Stack>
         </Stack>
           
-          <Stack>
+          <Stack spacing={4}> 
+          <AntSwitch 
+          onChange={()=>{
+          onToggleMode();
+          }} 
+          defaultChecked />
           <Avatar src={faker.image.avatar()} />
           </Stack>
           
